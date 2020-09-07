537ce3ca4b6b36e7fed387e2a87137edaebd6679
Mujhtech <mujeeb.muhideen@gmail.com>
2020-09-07 18:34:54 +0100

    add menu builder

---
+++ src/Events/BuildingMenu.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mujhtech\LaravelMentor\Events;
+
+use Mujhtech\LaravelMentor\Menu\Builder;
+
+class BuildingMenu
+{
+    /**
+     * The menu builder.
+     *
+     * @var Builder
+     */
+    public $menu;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Builder $menu
+     */
+    public function __construct(Builder $menu)
+    {
+        $this->menu = $menu;
+    }
+}
--- src/Mentor.php
+++ src/Mentor.php
@@ -4,6 +4,9 @@ namespace Mujhtech\LaravelMentor;
 
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Events\Dispatcher;
+use Mujhtech\LaravelMentor\Helpers\MenuItemHelper;
+use Mujhtech\LaravelMentor\Menu\Builder;
+use Mujhtech\LaravelMentor\Events\BuildingMenu;
 
 class Mentor {
 
@@ -43,11 +46,111 @@ class Mentor {
 
         // Fill the map of filters methods.
 
-        //$this->menuFilterMap = [
-           // 'sidebar'      => [$this, 'sidebarFilter'],
-            //'navbar-left'  => [$this, 'navbarLeftFilter'],
-            //'navbar-right' => [$this, 'navbarRightFilter'],
-            //'navbar-user'  => [$this, 'navbarUserMenuFilter'],
-        //];
+        $this->menuFilterMap = [
+            'sidebar'      => [$this, 'sidebarFilter'],
+            'navbar-left'  => [$this, 'navbarLeftFilter'],
+            'navbar-right' => [$this, 'navbarRightFilter'],
+            'navbar-user'  => [$this, 'navbarUserMenuFilter'],
+        ];
+    }
+
+
+    public function menu($filterToken = null)
+    {
+        if (empty($this->menu)) {
+            $this->menu = $this->buildMenu();
+        }
+
+        // Check for filter token.
+
+        if (isset($this->menuFilterMap[$filterToken])) {
+            return array_filter(
+                $this->menu,
+                $this->menuFilterMap[$filterToken]
+            );
+        }
+
+        // No filter token provided, return the complete menu.
+
+        return $this->menu;
+    }
+
+    /**
+     * Build the menu.
+     *
+     * @return array The set of menu items
+     */
+    protected function buildMenu()
+    {
+        // Create the menu builder instance.
+
+        $builder = new Builder($this->buildFilters());
+
+        // Dispatch the BuildingMenu event. Listeners of this event will fill
+        // the menu.
+
+        $this->events->dispatch(new BuildingMenu($builder));
+
+        // Return the set of menu items.
+
+        return $builder->menu;
+    }
+
+    /**
+     * Build the menu filters.
+     *
+     * @return array The set of filters that will apply on each menu item
+     */
+    protected function buildFilters()
+    {
+        return array_map([$this->container, 'make'], $this->filters);
+    }
+
+    /**
+     * Filter method used to get the sidebar menu items.
+     *
+     * @param mixed $item A menu item
+     * @return bool
+     */
+    private function sidebarFilter($item)
+    {
+        return MenuItemHelper::isSidebarItem($item);
+    }
+
+    /**
+     * Filter method used to get the top navbar left menu items.
+     *
+     * @param mixed $item A menu item
+     * @return bool
+     */
+    private function navbarLeftFilter($item)
+    {
+        if (MenuItemHelper::isSidebarItem($item)) {
+            return MenuItemHelper::isValidNavbarItem($item);
+        }
+
+        return MenuItemHelper::isNavbarLeftItem($item);
+    }
+
+    /**
+     * Filter method used to get the top navbar right menu items.
+     *
+     * @param mixed $item A menu item
+     * @return bool
+     */
+    private function navbarRightFilter($item)
+    {
+        return MenuItemHelper::isNavbarRightItem($item);
+    }
+
+    /**
+     * Filter method used to get the navbar user menu items.
+     *
+     * @param mixed $item A menu item
+     * @return bool
+     */
+    private function navbarUserMenuFilter($item)
+    {
+        return MenuItemHelper::isNavbarUserItem($item);
     }
 }
