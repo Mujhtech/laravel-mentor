ef09c11329f6599144211bb978d47a87b9f62abc
Mujhtech <mujeeb.muhideen@gmail.com>
2020-09-07 18:26:02 +0100

    add menu helper

--- src/Mentor.php
+++ src/Mentor.php
@@ -2,15 +2,52 @@
 
 namespace Mujhtech\LaravelMentor;
 
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Events\Dispatcher;
+
 class Mentor {
 
-    public function greet(String $sName)
+    protected $menu;
+
+    /**
+     * The array of menu filters. These filters will apply on each one of the
+     * menu items in order to transforms they in some way.
+     *
+     * @var array
+     */
+    protected $filters;
+
+    /**
+     * The events dispatcher.
+     *
+     * @var Dispatcher
+     */
+    protected $events;
+
+    /**
+     * The application service container.
+     *
+     * @var Container
+     */
+    protected $container;
+
+
+    protected $menuFilterMap;
+
+
+    public function __construct(array $filters, Dispatcher $events, Container $container)
     {
-        return 'Hi ' . $sName . '! How are you doing today?';
-    }
+        $this->filters = $filters;
+        $this->events = $events;
+        $this->container = $container;
 
-    public function getPath(String $path){
-        return __DIR__."/../$path";
-    }
+        // Fill the map of filters methods.
 
+        //$this->menuFilterMap = [
+           // 'sidebar'      => [$this, 'sidebarFilter'],
+            //'navbar-left'  => [$this, 'navbarLeftFilter'],
+            //'navbar-right' => [$this, 'navbarRightFilter'],
+            //'navbar-user'  => [$this, 'navbarUserMenuFilter'],
+        //];
+    }
 }
